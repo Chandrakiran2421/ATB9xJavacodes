17125b761416da2342bfc03dc8a7f377c7ec4036
chandrakiran2421 <ambatichandrakiran2421@gmail.com>
2024-10-07 23:01:46 +0530

    Add all files

--- src/OCT/exe03102024/Lab01.java
+++ src/OCT/exe03102024/Lab01.java
@@ -1,4 +1,7 @@
 package OCT.exe03102024;
 
 public class Lab01 {
+    public static void main(String[] args){
+        System.out.println("I am chandrakiran");
+    }
 }
---
+++ src/OCT/exe03102024/Lab02.java
@@ -0,0 +1,7 @@
+package OCT.exe03102024;
+
+public class Lab02 {
+    public static void main(String[] args) {
+        System.out.println("welcome");
+    }
+}
