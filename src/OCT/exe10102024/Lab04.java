29d93175ff2d82d50995541018fc28203121c2e3
chandrakiran2421 <ambatichandrakiran2421@gmail.com>
2024-10-14 18:22:50 +0530

    Add all files

---
+++ src/OCT/exe10102024/Lab04.java
@@ -0,0 +1,16 @@
+package OCT.exe10102024;
+
+public class Lab04 {
+    public  static void main (String [] args){
+        int a=10;
+        float b=3.45f;
+        char c='A';
+        String name="chandra";
+        boolean d= true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(name);
+        System.out.println(d);
+    }
+}
