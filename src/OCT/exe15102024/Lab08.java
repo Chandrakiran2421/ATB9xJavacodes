80a99b53345d231a1285d9ba7bfa5bcbf4661840
chandrakiran2421 <ambatichandrakiran2421@gmail.com>
2024-10-20 14:36:54 +0530

    Add all files

---
+++ src/OCT/exe15102024/Lab08.java
@@ -0,0 +1,11 @@
+package OCT.exe15102024;
+
+public class Lab08 {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println(++a + a++ + a++); //34
+
+        System.out.println(a); //13
+    }
+}
---
+++ src/OCT/exe15102024/Lab09.java
@@ -0,0 +1,9 @@
+package OCT.exe15102024;
+
+public class Lab09 {
+    public static void main(String[] args) {
+        int a = 20;
+
+        System.out.println(--a + a++ + a--); // 58
+    }
+}
