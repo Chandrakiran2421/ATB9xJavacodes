e3e6fbf4e3dd1c21060ea2ad36b1c02bb396d947
Chandrakiran2421 <ambatichandrakiran2421@gmail.com>
2024-10-28 06:47:28 +0530

    Push all files

---
+++ src/OCT/exe22102024/Lab19.java
@@ -0,0 +1,38 @@
+package OCT.exe22102024;
+import java.util.Scanner;
+public class Lab19 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        // Normalize the input by removing spaces and converting to lowercase
+        String normalizedInput = input.replaceAll("\\s+", "").toLowerCase();
+
+        if (isPalindrome(normalizedInput)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
+
+
