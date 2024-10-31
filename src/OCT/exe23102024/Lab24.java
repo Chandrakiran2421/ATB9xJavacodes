a48724c81bec2573a1e0d38b65beaf29743c3391
Chandrakiran2421 <ambatichandrakiran2421@gmail.com>
2024-10-31 16:12:00 +0530

    Push all files

---
+++ src/OCT/exe23102024/Lab24.java
@@ -0,0 +1,14 @@
+package OCT.exe23102024;
+import java.util.Scanner;
+public class Lab24 {
+    public static void main(String[] args) {
+greet();
+
+    }
+    static void greet (){
+        System.out.println("Hi, Iam chandra kiran");
+
+
+
+    }
+}
---
+++ src/OCT/exe23102024/Lab25.java
@@ -0,0 +1,19 @@
+package OCT.exe23102024;
+
+public class Lab25 {
+    public static void main(String[] args) {
+        greet();
+        String returned_value=welcome_funtion();
+        System.out.println(returned_value);
+
+    }
+    static void greet (){
+        System.out.println("Hi, Iam chandra kiran");
+
+    }
+    static String welcome_funtion(){
+        return "Welcome to the class";
+    }
+}
+
+
